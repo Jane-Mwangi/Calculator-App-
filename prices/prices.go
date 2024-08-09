b0d25e8c52f517b4d45c20f218f97c4100332fbf
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-09 14:13:36 +0300

    Add a method.

--- prices/prices.go
+++ prices/prices.go
@@ -1,15 +1,26 @@
 package prices
 
+import "fmt"
+
 type taxIncludePriceJob struct {
 	TaxRate          float64
 	InputPrices      []float64
 	taxIncludePrices map[string]float64
 }
 
+func (job taxIncludePriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%2f", price)] = price * (1 + job.TaxRate)
+	}
+	fmt.Println(result)
+}
+
 func NewTaxIncludedPriceJob(taxRate float64) *taxIncludePriceJob {
 
 	return &taxIncludePriceJob{
-		InputPrices: 	[]float64{10, 20, 30},
-		TaxRate: 		taxRate,
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
 	}
 }
