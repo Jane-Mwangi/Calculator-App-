681d6182849ad0f9bb7fd6bcb58363ac4eb6a5c4
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-09 13:59:44 +0300

    Add Constructor function.

--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,15 @@
 package prices
 
-type taxIncludePrices struct {
-	TaxRate float64
-	InputPrices []float64
+type taxIncludePriceJob struct {
+	TaxRate          float64
+	InputPrices      []float64
 	taxIncludePrices map[string]float64
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *taxIncludePriceJob {
+
+	return &taxIncludePriceJob{
+		InputPrices: 	[]float64{10, 20, 30},
+		TaxRate: 		taxRate,
+	}
 }
