9afa81944cb56852ae4c81a65bce1a878ec78cb9
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-11 14:01:02 +0300

    Add Struct tags.

--- prices/prices.go
+++ prices/prices.go
@@ -1,41 +1,37 @@
 package prices
 
 import (
-
 	"calculator/conversions"
 	"calculator/filemanager"
 	"fmt"
-	
 )
 
 type TaxIncludePriceJob struct {
-	IOManager 	  filemanager.FileManager
-	TaxRate          float64
-	InputPrices      []float64
-	TaxIncludePrices map[string]string
+	IOManager        filemanager.FileManager `json:"-"`
+	TaxRate          float64                 `json:"tax_rate"`
+	InputPrices      []float64               `json:"input_prices"`
+	TaxIncludePrices map[string]string       `json:"tax_include_prices"`
 }
 
 func (job *TaxIncludePriceJob) LoadData() {
 
-	lines,err :=job.IOManager.ReadLines()
+	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-
 	prices, err := conversions.StringsToFloat(lines)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		job.InputPrices = prices
-		
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
+	job.InputPrices = prices
+
+}
 
 func (job *TaxIncludePriceJob) Process() {
 
@@ -44,18 +40,18 @@ func (job *TaxIncludePriceJob) Process() {
 	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		taxIncludePrice:=price*(1+job.TaxRate)
-		result[fmt.Sprintf("%2f", price)] = fmt.Sprintf("%2f",taxIncludePrice)
+		taxIncludePrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%2f", price)] = fmt.Sprintf("%2f", taxIncludePrice)
 	}
-	job.TaxIncludePrices =result
+	job.TaxIncludePrices = result
 	job.IOManager.WriteResult(job)
-	
+
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager,taxRate float64) *TaxIncludePriceJob {
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludePriceJob {
 
 	return &TaxIncludePriceJob{
-		IOManager:  fm,
+		IOManager:   fm,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
